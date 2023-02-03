6488a6faad3acf59bd02dfb915f17b9f49f2d3bb
k-h0shikawa <81114182+k-h0shikawa@users.noreply.github.com>
2023-02-03 23:30:17 +0900

    単一の素材からメニューを返す関数を作成
    * 材料を取得できる機能を追加
    
    * リファクタリング
    
    * 入力からレシピを返す関数を作成する枠組み作成
    
    * README作成
    
    * POSTで素材を指定したときに、作成できるメニューを返す処理を作成
    
    * 不必要なコメントアウトを削除

--- src/main/java/com/example/whatdoieat/dao/MenuMapper.java
+++ src/main/java/com/example/whatdoieat/dao/MenuMapper.java
@@ -4,11 +4,12 @@ import java.util.List;
 import org.apache.ibatis.annotations.Mapper;
 
 import com.example.whatdoieat.entity.MenuEntity;
+import com.example.whatdoieat.entity.OwnIngredientEntity;
 
 @Mapper
-
 public interface MenuMapper {
     public List<MenuEntity> search();
 
-    public List<MenuEntity> canMakeMenus();
+    public List<MenuEntity> canMakeMenus(OwnIngredientEntity ownIngredientEntity);
+
 }
